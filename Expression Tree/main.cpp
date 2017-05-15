f3c80a1ae98c734162ec95f0f0c2092212b9377c
Nicholas <Nicck@Nicks-MacBook-Pro.local>
2017-05-15 09:51:03 -0400

    Initial Commit

---
+++ Expression Tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Expression Tree
+//
+//  Created by Nicholas on 5/15/17.
+//  Copyright © 2017 Nicholas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
